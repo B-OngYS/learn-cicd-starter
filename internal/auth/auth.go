ea33775289cd691800954626ed389b9a43f3899e
BforBenData <benedict.ongys@gmail.com>
2025-07-27 11:53:15 +0800

    Trying to break code again

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -14,8 +14,8 @@ func GetAPIKey(headers http.Header) (string, error) {
 	if authHeader == "" {
 		return "", ErrNoAuthHeaderIncluded
 	}
-	splitAuth := strings.Split(authHeader, "/")
-	if len(splitAuth) < 2 || splitAuth[0] != "ApiKey" {
+	splitAuth := strings.Split(authHeader, " ")
+	if len(splitAuth) < 2 || splitAuth[0] != "piKey" {
 		return "", errors.New("malformed authorization header")
 	}
 
